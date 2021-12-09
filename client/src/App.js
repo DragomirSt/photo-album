cce5904c8c3585258c56e146e180823eb1a06b58
DragomirSt <drago89@abv.bg>
2021-12-09 08:16:47 -0800

    add Login and Register routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation/Navigation';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <div className="App">
+        <Navigation />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
