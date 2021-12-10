6df01b6e02768a3c72f5c6b7c606369f8543aa15
DragomirSt <drago89@abv.bg>
2021-12-09 23:25:29 -0800

    set user context
    add user information about authorization in the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,39 @@
 
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+
+import { AuthContext } from './contexts/AuthContext';
 
 import Navigation from './components/Navigation/Navigation';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 
 function App() {
+
+  const [user, setUser] = useState({
+    _id: null,
+    email: null,
+    accessToken: null
+  });
+
+  const setUserSessionData = (authData) => {
+    setUser(authData);
+  }
+
   return (
     <>
-      <div className="App">
-        <Navigation />
-      </div>
-      <div>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </div>
+      <AuthContext.Provider value={{ user, setUserSessionData }}>
+        <div className="App">
+          <Navigation />
+        </div>
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+      </AuthContext.Provider>
     </>
   );
 }
