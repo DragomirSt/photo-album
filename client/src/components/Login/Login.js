ac1364ef04d6521da0d926dcbe1040d8ec0a707e
DragomirSt <drago89@abv.bg>
2021-12-09 23:27:49 -0800

    add user data from the server
    push user info and access token in the state on login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,8 +1,10 @@
 
-
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
 import * as authService from '../../servecies/auth';
 const Login = () => {
-
+    
+    const {setUserSessionData} = useContext(AuthContext);
     const loginHandler = (e) => {
         e.preventDefault();
         let formData = new FormData(e.target);
@@ -13,6 +15,7 @@ const Login = () => {
         authService.login(email, password)
             .then(authData => {
                 console.log(authData);
+                setUserSessionData(authData);
             })
             .catch(err => {
                 //Toddo: error handling
