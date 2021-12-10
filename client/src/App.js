31dea66344e1bb8cbcd51e941c194580875ee6de
DragomirSt <drago89@abv.bg>
2021-12-10 04:10:48 -0800

    add Logout

--- client/src/App.js
+++ client/src/App.js
@@ -8,22 +8,28 @@ import { AuthContext } from './contexts/AuthContext';
 import Navigation from './components/Navigation/Navigation';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Logout from './components/Logout/Logout';
+
+const initialUserState = {
+  _id: null,
+  email: null,
+  accessToken: null
+};
 
 function App() {
-
-  const [user, setUser] = useState({
-    _id: null,
-    email: null,
-    accessToken: null
-  });
+  const [user, setUser] = useState(initialUserState);
 
   const setUserSessionData = (authData) => {
     setUser(authData);
   }
 
+  const logoutUser = () => {
+    setUser(initialUserState);
+  }
+
   return (
     <>
-      <AuthContext.Provider value={{ user, setUserSessionData }}>
+      <AuthContext.Provider value={{ user, setUserSessionData, logoutUser }}>
         <div className="App">
           <Navigation />
         </div>
@@ -31,6 +37,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="logout" element={<Logout/>}/>
           </Routes>
         </div>
       </AuthContext.Provider>
