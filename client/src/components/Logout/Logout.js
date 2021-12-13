ae589674e636a39929200acf4738ba1edc845555
DragomirSt <drago89@abv.bg>
2021-12-13 05:07:55 -0800

    update Logout.js
    add navigation to login for now after successful logout

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -2,17 +2,20 @@
 import * as authService from '../../servecies/auth';
 import { AuthContext } from '../../contexts/AuthContext';
 
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
-    const {logoutUser} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { logoutUser } = useContext(AuthContext);
 
     useEffect(() => {
         authService.logout()
-        .then(() => {
-            logoutUser()
-        })
-    })
+            .then(() => {
+                logoutUser()
+                navigate('/login');
+            })
+    }, [])
     return null;
 }
 
