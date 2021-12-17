6f34fb53470129c33c07e36ad454277cf9eab3f2
DragomirSt <drago89@abv.bg>
2021-12-17 07:15:55 -0800

    add authmiddleware
    add middleware to verify the token, make route guard

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+
+const jwt = require('jsonwebtoken');
+const secretString = require('../utils/constants');
+
+exports.verifyToken = (req, res, next) => {
+    let token = req.headers['x-authorization'];
+
+    if (token) {
+        jwt.verify(token, secretString.SECRET, function (err, decodedToken) {
+            if (err) {
+                res.status(401).json({ message: 'You are not authorized' });
+                return;
+            }
+            req.user = decodedToken;
+            next();
+        });
+    } else {
+        next();
+    }
+};
+
+exports.isAuth = function (req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.status(401).json({ message: 'You are not authorized' });
+    }
+};
+
+
+
