76677d2299c9f76d30d3f86ba9d2e77bb6eab151
DragomirSt <drago89@abv.bg>
2021-12-17 07:31:57 -0800

    add link to create in the navigation

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -7,8 +7,10 @@ const Navagation = () => {
     return (
         <div className='topnav'>
             <nav>
+                <Link to="/"></Link>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
+                <Link to="/create">Create</Link>
                 <Link to="/logout">Logout</Link>
             </nav>
         </div>
