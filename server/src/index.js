807aef6f72decba37ea020a4807a9e0f7d56eda3
DragomirSt <drago89@abv.bg>
2021-12-17 07:13:57 -0800

    update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,17 +7,19 @@ const dataBase = require('./config/dataBase');
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config.json')[env];
 const router = require('./routes');
+const { verifyToken } = require('./middlewares/authMiddleware')
 
 
 app.use(cors());
 app.use(express.json());
+app.use(verifyToken);
 app.use(router);
 
-app.get('/api' , (req, res) => {
-    res.json({message: 'Server is running'})
+app.get('/api', (req, res) => {
+    res.json({ message: 'Server is running' })
 })
 
 dataBase(config.DB_CONNECTION_STRING)
-.then(() => {
-    app.listen(config.PORT, () => console.log('app is running on http://localhost:3030'))
-})
+    .then(() => {
+        app.listen(config.PORT, () => console.log('app is running on http://localhost:3030'))
+    })
