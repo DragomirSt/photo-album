8110b382bc1aeecf88cfd4480581feda3833a995
DragomirSt <drago89@abv.bg>
2021-12-17 07:33:22 -0800

    add photo service
    implement functionality to fetch data from server

---
+++ client/src/servecies/photo.js
@@ -0,0 +1,32 @@
+
+export const createCard = (data, token) => {
+
+    return fetch('/data/photos', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(data)
+    })
+        .then(result => {
+            return result
+        });
+
+};
+
+export const getAllPhotos = () => {
+
+    return fetch('/data/photos',{})
+        .then(res => res.json());
+
+}
+
+export const deletePhoto = (id) => {
+
+    return fetch(`/data/photos/${id}`, {
+        method: 'DELETE'
+    });
+};
+
+
