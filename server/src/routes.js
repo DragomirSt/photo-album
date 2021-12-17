359d5004ce0abda311c92535f4980b25f663a226
DragomirSt <drago89@abv.bg>
2021-12-17 07:10:35 -0800

    add photo card route

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const userControler = require('./controlers/userController');
+const photoControler = require('./controlers/photoCardsController');
 
 router.use('/users', userControler);
+router.use('/data/photos', photoControler);
 
-module.exports = router
+module.exports = router;
