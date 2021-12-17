1e45924bd2c769f8365ef5d227171ac832f9bc5e
DragomirSt <drago89@abv.bg>
2021-12-17 07:13:05 -0800

    add photo card service
    implement  service functionality with database

---
+++ server/src/servecies/photoCard.js
@@ -0,0 +1,18 @@
+
+const PhotoCard = require('../models/PhotoCard');
+
+exports.create = (data) => {
+    return PhotoCard.create({ ...data });
+};
+
+exports.getAll = () => {
+    return PhotoCard.find();
+};
+
+exports.deletePhoto = (id) => {
+    return PhotoCard.findByIdAndDelete(id);
+};
+
+exports.getOne = (id) => {
+    return PhotoCard.findById(id);
+};
