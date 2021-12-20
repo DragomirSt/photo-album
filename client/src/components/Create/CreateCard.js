ef55b41316dcdefacbeb5b0976a72f2fc49cc64b
DragomirSt <drago89@abv.bg>
2021-12-20 07:30:32 -0800

    Update create Card functionality

--- client/src/components/Create/CreateCard.js
+++ client/src/components/Create/CreateCard.js
@@ -12,41 +12,56 @@ const Create = () => {
 
     const createPhotoCard = (e) => {
         e.preventDefault();
+        const data = new FormData();
         let formData = new FormData(e.target);
 
         let name = formData.get('name');
         let genre = formData.get('genre');
         let imageUrl = formData.get('imageUrl');
 
-        photo.createCard({
-            name,
-            genre,
-            imageUrl
-        }, user.accessToken)
-        .then(res => {
-            navigate('/');
-        })
-       
+        data.append('name', name)
+        data.append('genre', genre);
+        data.append('imageUrl', imageUrl);
+        console.log(data)
+
+        photo.createCard(data, user.accessToken)
+            .then(res => {
+                navigate('/');
+            })
+
     };
 
 
     return (
-        <form className='create-form' method="POST" onSubmit={createPhotoCard}>
-            <fieldset className='create-fieldset'>
-                <legend>Create Photo Card</legend>
+        <div className='create-form'>
+            <form method="POST" onSubmit={createPhotoCard}>
+                <div className='add-form-text'>
+                    <h2>Add New Photo</h2>
+                </div>
+                <div className='create-label'>
+                    <h3>Name: </h3>
+                </div>
+                <div className='inputs'>
+                    <input type="text" name="name" className="name" placeholder="Name ...." />
+                </div>
+                <div className='genre-label'>
+                    <h3>Genre: </h3>
+                </div>
+                <div className='input-genre'>
+                    <input type="text" name="genre" className="genre" placeholder='Genre ....' />
+                </div>
+                <div className='image-label'>
+                    <h3>Photo: </h3>
+                </div>
+                <div className='image'>
+                    <input type="file" name="imageUrl" className="file"  />
+                </div>
 
-                <label htmlFor="email">Name</label>
-                <input type="text" name="name" placeholder="Photo name ..." />
+                <input className="button-submit" type="submit" value="Add New Photo" />
 
-                <label htmlFor="genre">Genre</label>
-                <input type="text" name="genre" placeholder="Photo name ..." />
+            </form>
+        </div>
 
-                <label htmlFor="image-url">Image Url</label>
-                <input type="text" name="imageUrl" placeholder="Image Url ... " />
-
-                <input className="button submit" type="submit" value="Create Card" />
-            </fieldset>
-        </form>
     )
 }
 
