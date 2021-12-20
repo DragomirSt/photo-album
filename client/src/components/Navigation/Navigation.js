c4cb34698616221f7178c962ead41ebe06634042
DragomirSt <drago89@abv.bg>
2021-12-20 08:02:10 -0800

    Update Navigation.js
    change the links in the navigation according to if there's a user logged in or not

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,18 +1,35 @@
 
 import './Navigation.css';
 
-import { Link } from 'react-router-dom'
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import { AuthContext } from '../../contexts/AuthContext';
+
 const Navagation = () => {
-   
+    const { user } = useContext(AuthContext);
+
+    const guestNavigation = (
+        <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+        </>
+    );
+    const userNavigation = (
+        <>
+            <Link to="/create">Create</Link>
+            <Link to="/logout">Logout</Link>
+        </>
+    );
+
     return (
         <div className='topnav'>
             <nav className='nav'>
                 <Link to="/"></Link>
                 <Link to="/all-photos">Photos</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-                <Link to="/create">Create</Link>
-                <Link to="/logout">Logout</Link>
+                {user.email
+                    ? userNavigation
+                    : guestNavigation}
             </nav>
         </div>
     )
