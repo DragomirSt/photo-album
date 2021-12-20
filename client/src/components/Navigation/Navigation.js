330abf3eadd28a3fbbcb585d5b1538088a32b072
DragomirSt <drago89@abv.bg>
2021-12-20 07:34:14 -0800

    Update Navigation.js

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,11 +3,12 @@ import './Navigation.css';
 
 import { Link } from 'react-router-dom'
 const Navagation = () => {
-
+   
     return (
         <div className='topnav'>
-            <nav>
+            <nav className='nav'>
                 <Link to="/"></Link>
+                <Link to="/all-photos">Photos</Link>
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
                 <Link to="/create">Create</Link>
