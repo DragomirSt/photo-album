c571c3b0395d3bc3141a564056e9bf061de41ffc
DragomirSt <drago89@abv.bg>
2021-12-22 07:41:33 -0800

    add update functionality to the service

--- client/src/servecies/photo.js
+++ client/src/servecies/photo.js
@@ -1,26 +1,24 @@
 
 export const createCard = (data, token) => {
-    
+
     return fetch('/data/photos', {
         method: 'POST',
         headers: {
-        'content-type': 'application/json',
-        'X-Authorization': token
+            'content-type': 'application/json',
+            'X-Authorization': token
         },
         body: JSON.stringify(data)
     })
         .then(result => {
             return result
         });
-
 };
 
 export const getAllPhotos = () => {
 
     return fetch('/data/photos', {})
         .then(res => res.json());
-
-}
+};
 
 export const deletePhoto = (id) => {
 
@@ -33,6 +31,20 @@ export const getCertainPhoto = (id) => {
 
     return fetch(`/data/photos/${id}`)
         .then(res => res.json());
-}
+};
+
+export const updatePhoto = (data, id) => {
+
+    return fetch(`/data/photos/${id}`, {
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(result => {
+            return result;
+        });
+};
 
 
