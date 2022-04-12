c870f47cd307484e0f893e395eee28ea25a5ed6c
DragomirSt <drago89@abv.bg>
2022-04-12 05:00:26 -0700

    removed the app.css stylings

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 
-import './App.css';
-
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 
+import { Toaster } from 'react-hot-toast';
 import { AuthContext } from './contexts/AuthContext';
 
 import Navigation from './components/Navigation/Navigation';
@@ -16,7 +15,7 @@ import DeleteComponent from './components/Delete/DeleteComponent';
 import Details from './components/Details/Details';
 import Edit from './components/Edit/Edit';
 import MyPhotos from './components/MyPhotos/MyPhotos';
-import Comment from './components/Comment/Comment'
+import Comment from './components/Comment/Comment';
 
 const initialUserState = {
   _id: null,
@@ -38,10 +37,12 @@ function App() {
 
   return (
     <AuthContext.Provider value={{ user, setUserSessionData, logoutUser }}>
-      <div>
         <div className='app'>
+          <Toaster containerStyle={{
+            right: 700,
+            top: 35
+          }} />
           <Navigation />
-
           <main className='site-content'>
             <Routes>
               <Route path="/" element={<AllPhotos />} />
@@ -50,16 +51,14 @@ function App() {
               <Route path="/register" element={<Register />} />
               <Route path="/create" element={<Create />} />
               <Route path="/logout" element={<Logout />} />
-              <Route path="/delete/:id" element={<DeleteComponent />} />
-              <Route path="/details/:id" element={<Details />} />
+              <Route path="/details/:id" element={<Details />} /><Route />
               <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/delete/:id" element={<DeleteComponent />} />
               <Route path="/my-photos" element={<MyPhotos />} />
-              <Route path="/comments/:id" element={<Comment/>} />
+              <Route path="/comment/:id" element={<Comment />} />
             </Routes>
           </main>
         </div>
-      </div>
-
     </AuthContext.Provider>
   );
 }
