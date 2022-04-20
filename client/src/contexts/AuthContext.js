ac12391b5e36240c75ae663c00acdf529246c237
DragomirSt <drago89@abv.bg>
2022-04-20 03:02:51 -0700

    setup the AuthContext
    moved the context functionality out of App.js to a separate file

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,3 +1,27 @@
+
 import { createContext } from 'react';
+import { useState } from 'react';
 
 export const AuthContext = createContext();
+const initialUserState = {
+    _id: null,
+    email: null,
+    accessToken: null
+};
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(initialUserState);
+
+    const setUserSessionData = (authData) => {
+        setUser(authData);
+    };
+    const logoutUser = () => {
+        setUser(initialUserState);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, setUserSessionData, logoutUser }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
