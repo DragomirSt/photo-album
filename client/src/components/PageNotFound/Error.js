485c909a3c50032236fff9bab2e40681aa1d486f
DragomirSt <drago89@abv.bg>
2022-04-21 05:53:51 -0700

    add error 404 page

---
+++ client/src/components/PageNotFound/Error.js
@@ -0,0 +1,13 @@
+
+import './Error.css'
+
+const PageNotFound = () => {
+    return (
+        <div className='error-div'>
+            <h1>404</h1>
+            <h1>Page not found</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;
